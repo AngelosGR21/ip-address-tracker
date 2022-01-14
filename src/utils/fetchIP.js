357924b7acc4264f1d245d13da4fd859b1e799ad
Angelos Grigoriu <77233810+AngelosGR21@users.noreply.github.com>
2022-01-14 12:53:21 +0000

    Updating key

--- src/utils/fetchIP.js
+++ src/utils/fetchIP.js
@@ -8,8 +8,8 @@ const fetchIP = async (ip, setIPData, setPosition, setLoading) => {
   try {
     const response = await axios.get(
       domain.test(ip) === true
-        ? `https://geo.ipify.org/api/v1?apiKey=at_HQrZnnGYyKxrgfJ3QzjX96yHJedqN&domain=${ip}`
-        : `https://geo.ipify.org/api/v1?apiKey=at_HQrZnnGYyKxrgfJ3QzjX96yHJedqN&ipAddress=${ip}`
+        ? `https://geo.ipify.org/api/v1?apiKey=at_LKMyv1Cc99C2QJfcq9r4s828Z48rA&domain=${ip}`
+        : `https://geo.ipify.org/api/v1?apiKey=at_LKMyv1Cc99C2QJfcq9r4s828Z48rA&ipAddress=${ip}`
     );
     setIPData(response.data);
     setPosition([response.data.location.lat, response.data.location.lng]);
